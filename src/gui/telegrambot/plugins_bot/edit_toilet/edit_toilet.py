86960fc95b91bfd8b94b13a8342ccaf1ad938acb
Ilnur Sayfutdinov <ilnursoft@gmail.com>
2021-08-16 22:05:44 +0300

    rename модуля редактирования туалета без реализации

--- src/gui/telegrambot/plugins_bot/edit_toilet/edit_toilet.py
+++ src/gui/telegrambot/plugins_bot/edit_toilet/edit_toilet.py
@@ -8,6 +8,6 @@ from telethon import events
 # import json
 
 
-@tlgbot.on(events.NewMessage(chats=tlgbot.settings.get_all_user_id(), pattern='/addtoilet'))
+@tlgbot.on(events.NewMessage(chats=tlgbot.settings.get_all_user_id(), pattern='/edit_toilet'))
 async def add_toilet(event):
     await event.respond("Редактирование описания туалета.")
