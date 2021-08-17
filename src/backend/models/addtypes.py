3c9bc4fd59a186a68b0b778150d8e316f12f56f9
ILnur Saifutdinov <ilnursoft@gmail.com>
2021-08-17 12:37:19 +0300

    add type GeoCoords

--- src/backend/models/addtypes.py
+++ src/backend/models/addtypes.py
@@ -29,3 +29,29 @@ class Rating(Enum):
     soso = 3
     good = 4
     recommend = 5
+
+
+class GeoCoords:
+    """
+    класс для хранения географических координат
+    """
+
+    def __init__(self, lat: float = 0.0, long: float = 0.0) -> None:
+        self._latitude: float = 0.0  # долгота
+        self._longitude: float = 0.0  # широта
+
+    @property
+    def lat(self) -> float:
+        return self._latitude
+
+    @lat.setter
+    def lat(self, new_lat: float) -> None:
+        self._latitude = new_lat
+
+    @property
+    def long(self) -> float:
+        return self._longitude
+
+    @long.setter
+    def long(self, new_long: float) -> None:
+        self._longitude = new_long
